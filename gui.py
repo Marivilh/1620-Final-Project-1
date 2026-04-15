bd3fd4dd36b8600d90581524b61cb6b64926baa0
Marivilh <fmabed000@gmail.com>
2026-04-15 14:18:14 -0500

    pissy comments

--- gui.py
+++ gui.py
@@ -6,3 +6,7 @@ app = QtWidgets.QApplication(sys.argv)
 window = uic.loadUi("gui.ui")
 window.show()
 app.exec()
+
+# FIXME: try to get pyuic6 to work, until then this is needed
+# it really pissed me off trying to get pyuic6 to work so i just went with this
+# that i foun on this website https://www.pythonguis.com/tutorials/pyqt6-first-steps-qt-designer/
